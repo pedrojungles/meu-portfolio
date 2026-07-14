145a59d51772ec71f09df1e6d1c11ce416be154d
Pedro Henrique Jungles <phjunglestk@gmail.com>
2026-07-14 17:53:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const botao = document.getElementById("btn-tema");
+document.addEventListener("DOMContentLoaded", function() {
+  const botao = document.getElementById("btn-tema");
 
-botao.addEventListener("click", function() {
-  document.body.classList.toggle("dark");
+  botao.addEventListener("click", function() {
+    document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    botao.textContent = "☀️ Modo claro";
-  } else {
-    botao.textContent = "🌙 Modo escuro";
-  }
+    if (document.body.classList.contains("dark")) {
+      botao.textContent = "☀️ Modo claro";
+    } else {
+      botao.textContent = "🌙 Modo escuro";
+    }
+  });
 });
